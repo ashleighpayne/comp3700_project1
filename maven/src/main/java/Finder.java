e5cbee6461babbc70ff84f97e5e977811d7b5d58
Ashleigh Payne <ashleigh.payne@adtran.com>
2019-08-22 23:08:16 -0500

    added comments

--- maven/src/main/java/Finder.java
+++ maven/src/main/java/Finder.java
@@ -1,11 +1,22 @@
+//Author: Ashleigh Payne
+//aap0035
+//COMP 3700
+
 public class Finder {
     
+    //Method to find the largest number in the array
     java.lang.Integer findMax(int[] intArray) {
+
+        //Logic to handle inappropriate array submissions
         if (intArray == null || intArray.length == 0) {
             return null;
         }
+
+        //Logic to handle appropriate array submissions
         else {
-            int count = 0; 
+            int count = 1; 
+            //Starting with the first number is arbitrary, could start with any number
+            //However, this means we can skip the comparison intArray[0], and count starts at 1
             int currentMax = intArray[0];
             while (count < intArray.length) {
                 if(intArray[count] > currentMax) {
@@ -17,13 +28,20 @@ public class Finder {
         }
     }
 
+    //Method to find the smallest number in the array
     java.lang.Integer findMin(int[] intArray) {
+
+        //Logic to handle inappropriate array submissions
         if (intArray == null || intArray.length == 0) {
             return null;
         }
+
+        //Logic to handle appropriate array submissions
         else {
-            int count = 0; 
+            int count = 1; 
             int currentMin = intArray[0];
+            //Starting with the first number is arbitrary, could start with any number
+            //However, this means we can skip the comparison intArray[0], and count starts at 1
             while (count < intArray.length) {
                 if(intArray[count] < currentMin) {
                     currentMin = intArray[count];
